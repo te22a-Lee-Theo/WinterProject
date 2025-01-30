c2aff73945d76b0035d26cfa28552904f09b4dfe
Theo Lee <theo.leehao@elev.ga.ntig.se>
2025-01-30 16:11:33 +0100

    Item + character class

--- WinterProject/Player.cs
+++ WinterProject/Player.cs
@@ -1,6 +1,11 @@
 public class Player: Character
 {
-    public List<Item> Items = new();
+    public Player()
+    {
+        Name = "";
+    }
+    // public string Name;
+    public List<Items> Items = new();
 
 
     public void Attack()
@@ -14,5 +19,17 @@ public class Player: Character
     public void Display()
     {
 
+    }
+    public string GetName()
+    {
+        Name = Console.ReadLine();
+
+        while (string.IsNullOrEmpty(Name))
+        {
+            System.Console.WriteLine("You're not getting away without giving me your name");
+            Name = Console.ReadLine();
+        }
+
+        return Name;
     }
 }
--- WinterProject/Program.cs
+++ WinterProject/Program.cs
@@ -1,4 +1,31 @@
 ﻿
+Character one = new Player();
+
+string Answer;
+
+Console.WriteLine("Welcome to The Basement");
+System.Console.WriteLine("I see you too have become a victim for sacrifice");
+System.Console.WriteLine("Well then. Do you remember your name? (y/n)");
+Answer = Console.ReadLine().ToLower();
+
+    if (Answer == "y")
+    {
+        System.Console.WriteLine("Then, mind telling me your name?");
+        System.Console.WriteLine("(Enter a name)");
+    }
+    else if (Answer == "n")
+    {
+        System.Console.WriteLine("That's a shame. Feel free to choose a new one");
+        System.Console.WriteLine("(Enter a name)");
+    }
+    else
+    {
+        System.Console.WriteLine("Well, whether you like it or not you don't have choise but to state your name");
+        System.Console.WriteLine("(Enter a name)");
+    }
+
+string dwdw = Player.GetName();
+System.Console.WriteLine(dwdw);
 
 
 Console.ReadLine();
